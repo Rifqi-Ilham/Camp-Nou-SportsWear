f3e625448392e9dc4edfba99d154dfd5e9f19c43
Rifqi-Ilham <rifqiilham888@gmail.com>
2025-09-10 01:45:58 +0700

    update gitginore

--- campnou_sportswear/settings.py
+++ campnou_sportswear/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -139,8 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
